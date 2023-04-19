7e6a16b38ad2d42ace51e77bb3679397afaecff4
Kiran Prajapati <officeofkiran@gmail.com>
2023-04-19 16:30:50 +0530

    2.3.1

--- geodir-multilingual.php
+++ geodir-multilingual.php
@@ -11,7 +11,7 @@
  * Plugin Name:       GeoDirectory Multilingual
  * Plugin URI:        https://wpgeodirectory.com/downloads/wpml-multilingual/
  * Description:       Allows running your directory fully multilingual with GeoDirectory and WPML.
- * Version:           2.3
+ * Version:           2.3.1
  * Requires at least: 4.9
  * Requires PHP:      5.6
  * Author:            AyeCode Ltd
@@ -29,7 +29,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'GEODIR_MULTILINGUAL_VERSION' ) ) {
-	define( 'GEODIR_MULTILINGUAL_VERSION', '2.3' );
+	define( 'GEODIR_MULTILINGUAL_VERSION', '2.3.1' );
 }
 
 if ( ! defined( 'GEODIR_MULTILINGUAL_MIN_CORE' ) ) {
